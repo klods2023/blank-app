74e0614a9d1714116d59a3668d48b756ae941ac5
klods2023 <klods2010@yandex.ru>
2024-08-08 00:38:12 +0300

    1 commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Lроопроet's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
